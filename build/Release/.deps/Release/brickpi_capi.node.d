7abf0a810ed644638c0986954a0ad923323ff029
luc prieur <luc.prieur.ing@gmail.com>
2015-05-01 16:29:40 -0400

    remiving build directory. Uneeded

--- build/Release/.deps/Release/brickpi_capi.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/brickpi_capi.node := rm -rf "Release/brickpi_capi.node" && cp -af "Release/obj.target/brickpi_capi.node" "Release/brickpi_capi.node"
--- build/Release/.deps/Release/obj.target/brickpi_capi/c_brickpi.o.d
+++
@@ -1,6 +0,0 @@
-cmd_Release/obj.target/brickpi_capi/c_brickpi.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/0.10.28/src -I/home/pi/.node-gyp/0.10.28/deps/uv/include -I/home/pi/.node-gyp/0.10.28/deps/v8/include  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/brickpi_capi/c_brickpi.o.d.raw  -c -o Release/obj.target/brickpi_capi/c_brickpi.o ../c_brickpi.c
-Release/obj.target/brickpi_capi/c_brickpi.o: ../c_brickpi.c ../tick.h \
- ../BrickPi.h
-../c_brickpi.c:
-../tick.h:
-../BrickPi.h:
